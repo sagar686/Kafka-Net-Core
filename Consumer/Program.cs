2f69d68ad426988a6f8d88af28312ac15045ac26
Sagar Sangvekar <sag61086@gmail.com>
2023-07-02 13:50:05 +0530

    Add project files.

---
+++ Consumer/Program.cs
@@ -0,0 +1,47 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Threading;
+
+
+class Consumer
+{
+
+    static void Main()
+    {
+        var config = new ConsumerConfig();
+        config.BootstrapServers = "localhost:9092";
+        config.GroupId = "kafka-dotnet-getting-started";
+        config.AutoOffsetReset = AutoOffsetReset.Earliest;
+
+        const string topic = "purchases";
+
+        CancellationTokenSource cts = new CancellationTokenSource();
+        Console.CancelKeyPress += (_, e) =>
+        {
+            e.Cancel = true; // prevent the process from terminating.
+            cts.Cancel();
+        };
+
+        using (var consumer = new ConsumerBuilder<string, string>(
+            config).Build())
+        {
+            consumer.Subscribe(topic);
+            try
+            {
+                while (true)
+                {
+                    var cr = consumer.Consume(cts.Token);
+                    Console.WriteLine($"Consumed event from topic {topic} with key {cr.Message.Key,-10} and value {cr.Message.Value}");
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                // Ctrl-C was pressed.
+            }
+            finally
+            {
+                consumer.Close();
+            }
+        }
+    }
+}
